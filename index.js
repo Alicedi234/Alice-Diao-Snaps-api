8f8855aca96ef8f124492942d25d9d39986e297b
Yawen Diao <yawendiao@gmail.com>
2025-02-26 14:13:25 -0500

    index setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+//define the port
+const port = process.env.PORT ?? 5050;
+
+//create an express "instance"
+const app = express();
+
+//enable CORS
+app.use(cors());
